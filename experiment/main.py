71f652f70569fc5f8b152229d09882df4565b434
doujiang-zheng <1825597835@qq.com>
2020-03-25 17:01:47 +0800

    Train test split & unseen nodes, edges stats in test set.

--- experiment/main.py
+++ experiment/main.py
@@ -59,7 +59,45 @@ def to_dataframe(fname=["ia-contact.edges"]):
     pass
 
 
-def train_test_split():
+def train_test_split(train_ratio=0.75, project_dir="../../ctdne_data/", store_dir="../../"):
+    # nodes, edges, d_avg, d_max, timespan(days) 
+    fname = [f for f in os.listdir(project_dir) if f.endswith("csv")]
+    fname = sorted(fname)
+    fpath = [project_dir + f for f in fname]
+    for name, path in zip(fname, fpath):
+        print("*****{}*****".format(name))
+        df = pd.read_csv(path)
+        df = df.sort_values(by="timestamp").reset_index(drop=True)
+        train_end_idx = int(len(df) * train_ratio)
+        # df.loc contains both start and stop
+        train_df = df.iloc[:train_end_idx]#.reset_index(drop=True)
+        test_df = df.iloc[train_end_idx:].reset_index(drop=True)
+        test_df["label"] = 1
+        
+        train_nodes = set(train_df["from_node_id"]).union(train_df["to_node_id"])
+        test_nodes = set(test_df["from_node_id"]).union(test_df["to_node_id"])
+        unseen_nodes = test_nodes - train_nodes
+        from_edges = test_df["from_node_id"].apply(lambda x: x in unseen_nodes)
+        to_edges = test_df["to_node_id"].apply(lambda x: x in unseen_nodes)
+        edges = np.logical_or(from_edges, to_edges)
+        print("unseen {} nodes, {} edges in test set".format(len(unseen_nodes), sum(edges)))
+
+        nodes = list(set(df["from_node_id"]).union(df["to_node_id"]))
+        node2id = {key: idx for idx, key in enumerate(nodes)}
+        neg_test_df = test_df.copy().reset_index(drop=True)
+        neg_test_df["label"] = 0
+        for index, row in enumerate(test_df.itertuples()):
+            pos_idx = node2id[row.to_node_id]
+            # swap the positive index with the last element
+            nodes[-1], nodes[pos_idx] = nodes[pos_idx], nodes[-1]
+            neg_idx = np.random.choice(len(nodes) - 1)
+            neg_test_df.loc[index, "to_node_id"] = nodes[neg_idx]
+            # swap the positive index with the last element
+            nodes[-1], nodes[pos_idx] = nodes[pos_idx], nodes[-1]
+        test_df.append(neg_test_df, ignore_index=True)
+        test_df = test_df.sort_values(by="timestamp")
+        train_df.to_csv("{}/train_data/{}".format(store_dir, name), index=None)
+        test_df.to_csv("{}/test_data/{}".format(store_dir, name), index=None)
     pass
 
 
@@ -79,7 +117,8 @@ def predict(X, y):
     pass
 
 if __name__ == "__main__":
-    data_stats()
+    # data_stats()
+    train_test_split()
     pass
         
     
