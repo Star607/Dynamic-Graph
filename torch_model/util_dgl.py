0f3ba6b1cfc3dd44fec5be2cab212bcdf44e08d9
doujiang-zheng <1825597835@qq.com>
2020-07-20 17:00:33 +0800

    Check dgl utilities, including graph construction, message passing, padding at reduce_func to get various node embeddings, subgraph data and graph.local_var, group_apply_edges, src_feat_conv and dst_feat_conv, one_hop_conv for 2E node embeddings.

--- torch_model/util_dgl.py
+++ torch_model/util_dgl.py
@@ -282,24 +282,6 @@ def main():
     print(src_feat.shape, dst_feat.shape)
 
 
-class CrossEntropyLoss(nn.Module):
-    def forward(self, block_outputs, pos_graph, neg_graph):
-        with pos_graph.local_scope():
-            pos_graph.ndata['h'] = block_outputs
-            pos_graph.apply_edges(fn.u_dot_v('h', 'h', 'score'))
-            pos_score = pos_graph.edata['score']
-        with neg_graph.local_scope():
-            neg_graph.ndata['h'] = block_outputs
-            neg_graph.apply_edges(fn.u_dot_v('h', 'h', 'score'))
-            neg_score = neg_graph.edata['score']
-
-        score = th.cat([pos_score, neg_score])
-        label = th.cat(
-            [th.ones_like(pos_score), th.zeros_like(neg_score)]).long()
-        loss = F.binary_cross_entropy_with_logits(score, label.float())
-        return loss
-
-
 def nodeflow_test():
     pass
 
