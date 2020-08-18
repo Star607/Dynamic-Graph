f54dc08c44dc436ea980077c3303e52c4fe19abd
doujiang-zheng <1825597835@qq.com>
2020-08-18 15:17:43 +0800

    Add thread parallel for upper_bound_full.

--- torch_model/upper_bound_extension/upper_bound.cpp
+++ torch_model/upper_bound_extension/upper_bound.cpp
@@ -1,4 +1,5 @@
 #include <torch/extension.h>
+
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -7,29 +8,40 @@
 
 torch::Tensor upper_bound(const torch::Tensor &bucket_adj);
 torch::Tensor upper_bound_par(const torch::Tensor &bucket_adj);
-void sub_upper_bound(const torch::Tensor &bucket_adj, torch::Tensor &ans, const int row);
-torch::Tensor upper_bound_full(const torch::Tensor &nodes, const torch::Tensor &etime);
-torch::Tensor reference_lower_bound(const torch::Tensor &refer_adj, const torch::Tensor &bucket_adj);
+void sub_upper_bound(const torch::Tensor &bucket_adj, torch::Tensor &ans,
+                     const int row);
+torch::Tensor upper_bound_full(const torch::Tensor &nodes,
+                               const torch::Tensor &etime,
+                               const torch::Tensor &degrees);
+torch::Tensor upper_bound_full_par(const torch::Tensor &nodes,
+                                   const torch::Tensor &etime,
+                                   const torch::Tensor &degrees);
+void sub_upper_bound_full(const torch::Tensor &etime, torch::Tensor &ans,
+                          const int deg, const int idx);
+torch::Tensor reference_lower_bound(const torch::Tensor &refer_adj,
+                                    const torch::Tensor &bucket_adj);
 
-torch::Tensor upper_bound(const torch::Tensor &bucket_adj)
-{
-    /*  First of all, the last dimension of bucket_adj must be non-decreasing. We compute the upper_bound of each element along the last dimension.
-    *   The return result will be the same shape as input `bucket_adj`.
-    */
+torch::Tensor upper_bound(const torch::Tensor &bucket_adj) {
+    /*  First of all, the last dimension of bucket_adj must be
+     * non-decreasing. We compute the upper_bound of each element along the
+     * last dimension. The return result will be the same shape as input
+     * `bucket_adj`.
+     */
     using namespace torch::indexing;
     // We assume the bucket_adj is 2D tensor.
     auto sizes = bucket_adj.sizes();
     auto dim = sizes[1];
-    // Default upper_bound is the array length, which means itself is the largest element.
+    // Default upper_bound is the array length, which means itself is the
+    // largest element.
     auto ans = torch::full(sizes, dim, torch::kInt64);
-    for (auto i = 0; i < sizes[0]; i++)
-    {
+    for (auto i = 0; i < sizes[0]; i++) {
         auto ub = 1;
-        for (auto j = 0; j < dim && ub < dim; j++)
-        {
-            // Attention! If use `bucket_adj[i][ub].item<float>()` to get float value, the float precision cannot be guaranteed, so we directly get .item<bool>().
-            while (ub < dim && (bucket_adj[i][ub] <= bucket_adj[i][j]).item<bool>())
-            {
+        for (auto j = 0; j < dim && ub < dim; j++) {
+            // Attention! If use `bucket_adj[i][ub].item<float>()` to get
+            // float value, the float precision cannot be guaranteed, so we
+            // directly get .item<bool>().
+            while (ub < dim &&
+                   (bucket_adj[i][ub] <= bucket_adj[i][j]).item<bool>()) {
                 ub += 1;
             }
             ans.index_put_({i, j}, ub);
@@ -38,14 +50,13 @@ torch::Tensor upper_bound(const torch::Tensor &bucket_adj)
     return ans;
 }
 
-torch::Tensor upper_bound_par(const torch::Tensor &bucket_adj)
-{
+torch::Tensor upper_bound_par(const torch::Tensor &bucket_adj) {
     // We assume the bucket_adj is 2D tensor.
     auto sizes = bucket_adj.sizes();
     auto dim = sizes[1];
-    // Default upper_bound is the array length, which means itself is the largest element.
-    // auto processor_count = std::thread::hardware_concurrency();
-    // if (processor_count == 0)
+    // Default upper_bound is the array length, which means itself is the
+    // largest element. auto processor_count =
+    // std::thread::hardware_concurrency(); if (processor_count == 0)
     //     processor_count = 1;
     // auto ans = torch::full(sizes, dim, torch::kInt64);
     // at::parallel_for(0, sizes[0], 1, [&](int64_t begin, int64_t end) {
@@ -55,8 +66,11 @@ torch::Tensor upper_bound_par(const torch::Tensor &bucket_adj)
     //         auto ub = 1;
     //         for (auto j = 0; j < dim && ub < dim; j++)
     //         {
-    //             // Attention! If use `bucket_adj[row][ub].item<float>()` to get float value, the float precision cannot be guaranteed, so we directly get .item<bool>().
-    //             while (ub < dim && (bucket_adj[row][ub] <= bucket_adj[row][j]).item<bool>())
+    //             // Attention! If use `bucket_adj[row][ub].item<float>()`
+    //             to get float value, the float precision cannot be
+    //             guaranteed, so we directly get .item<bool>(). while (ub <
+    //             dim && (bucket_adj[row][ub] <=
+    //             bucket_adj[row][j]).item<bool>())
     //             {
     //                 ub += 1;
     //             }
@@ -66,36 +80,151 @@ torch::Tensor upper_bound_par(const torch::Tensor &bucket_adj)
     // });
     std::vector<std::thread> workers;
     auto ans = torch::full(sizes, dim, torch::kInt64);
-    for (auto i = 0; i < sizes[0]; i++)
-    {
-        workers.push_back(std::thread(sub_upper_bound, std::ref(bucket_adj), std::ref(ans), i));
+    for (auto i = 0; i < sizes[0]; i++) {
+        workers.push_back(std::thread(sub_upper_bound, std::ref(bucket_adj),
+                                      std::ref(ans), i));
         // sub_upper_bound(bucket_adj, ans, i);
     }
-    for (std::thread &t : workers)
-    {
+    for (std::thread &t : workers) {
         if (t.joinable())
             t.join();
     }
     return ans;
 }
 
-void sub_upper_bound(const torch::Tensor &bucket_adj, torch::Tensor &ans, const int row)
-{
+void sub_upper_bound(const torch::Tensor &bucket_adj, torch::Tensor &ans,
+                     const int row) {
     auto dim = bucket_adj.sizes()[1];
     auto ub = 1;
-    for (auto j = 0; j < dim && ub < dim; j++)
-    {
-        // Attention! If use `bucket_adj[row][ub].item<float>()` to get float value, the float precision cannot be guaranteed, so we directly get .item<bool>().
-        while (ub < dim && (bucket_adj[row][ub] <= bucket_adj[row][j]).item<bool>())
-        {
+    for (auto j = 0; j < dim && ub < dim; j++) {
+        // Attention! If use `bucket_adj[row][ub].item<float>()` to get
+        // float value, the float precision cannot be guaranteed, so we
+        // directly get .item<bool>().
+        while (ub < dim &&
+               (bucket_adj[row][ub] <= bucket_adj[row][j]).item<bool>()) {
             ub += 1;
         }
         ans.index_put_({row, j}, ub);
     }
 }
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
-{
+#ifndef NDEBUG
+#define ASSERT(condition, message)                                             \
+    do {                                                                       \
+        if (!(condition)) {                                                    \
+            std::cerr << "Assertion `" #condition "` failed in " << __FILE__   \
+                      << " line " << __LINE__ << ": " << message << std::endl; \
+            std::terminate();                                                  \
+        }                                                                      \
+    } while (false)
+#else
+#define ASSERT(condition, message)                                             \
+    do {                                                                       \
+    } while (false)
+#endif
+
+torch::Tensor upper_bound_full(const torch::Tensor &nodes,
+                               const torch::Tensor &etime,
+                               const torch::Tensor &degrees) {
+    auto size = nodes.sizes()[0];
+    ASSERT((size == etime.sizes()[0]), "The reference node Tensor must be "
+                                       "the same size as the edge Tensor.");
+    ASSERT((size == degrees.sum().item<int>()),
+           "The reference node Tensor must be equal to the degree Tensor "
+           "sum.");
+    auto num_node = degrees.sizes()[0];
+    auto ans = torch::full(nodes.sizes(), size, torch::kInt64);
+    auto idx = 0;
+    for (auto i = 0; i < num_node; i++) {
+        auto ub = 1;
+        auto deg = degrees[i].item<int>();
+        for (auto j = 0; j < deg; j++) {
+            // Attention! If use `bucket_adj[row][ub].item<float>()` to get
+            // float value, the float precision cannot be guaranteed, so we
+            // directly get .item<bool>().
+            while (ub < deg &&
+                   (etime[idx + ub] <= etime[idx + j]).item<bool>()) {
+                ub += 1;
+            }
+            ans.index_put_({idx + j}, ub);
+        }
+        idx += degrees[i].item<int>();
+    }
+    return ans;
+}
+
+torch::Tensor upper_bound_full_par(const torch::Tensor &nodes,
+                                   const torch::Tensor &etime,
+                                   const torch::Tensor &degrees) {
+    // auto size = nodes.sizes()[0];
+    // ASSERT((size == etime.sizes()[0]), "The reference node Tensor must be
+    // the same size as the edge Tensor."); ASSERT((size ==
+    // degrees.sum().item<int>()), "The reference node Tensor must be equal
+    // to the degree Tensor sum.");
+    auto size = etime.sizes()[0];
+    auto num_node = degrees.sizes()[0];
+    auto ans = torch::full(etime.sizes(), size, torch::kInt64);
+    auto idx = 0;
+    std::vector<std::thread> workers;
+
+    for (auto i = 0; i < num_node; i++) {
+        auto deg = degrees[i].item<int>();
+        workers.push_back(std::thread(sub_upper_bound_full, std::ref(etime),
+                                      std::ref(ans), deg, idx));
+        idx += deg;
+    }
+    for (std::thread &t : workers) {
+        if (t.joinable())
+            t.join();
+    }
+    return ans;
+}
+
+void sub_upper_bound_full(const torch::Tensor &etime, torch::Tensor &ans,
+                          const int deg, const int idx) {
+    auto ub = 1;
+    for (auto j = 0; j < deg; j++) {
+        while (ub < deg && (etime[idx + ub] <= etime[idx + j]).item<bool>()) {
+            ub += 1;
+        }
+        ans.index_put_({idx + j}, ub);
+    }
+}
+
+torch::Tensor reference_lower_bound(const torch::Tensor &refer_adj,
+                                    const torch::Tensor &bucket_adj) {
+    /*  First of all, the last dimension of bucket_adj must be
+     * non-decreasing. We compute the upper_bound of each element along the
+     * last dimension. The return result will be the same shape as input
+     * `bucket_adj`.
+     */
+    using namespace torch::indexing;
+    // We assume the bucket_adj is 2D tensor.
+    auto sizes = bucket_adj.sizes();
+    auto dim = sizes[1];
+    // Default upper_bound is the array length, which means itself is the
+    // largest element.
+    auto ans = torch::full(sizes, dim, torch::kInt64);
+    for (auto i = 0; i < sizes[0]; i++) {
+        auto ub = 1;
+        for (auto j = 0; j < dim && ub < dim; j++) {
+            // Attention! If use `bucket_adj[i][ub].item<float>()` to get
+            // float value, the float precision cannot be guaranteed, so we
+            // directly get .item<bool>().
+            while (ub < dim &&
+                   (bucket_adj[i][ub] <= bucket_adj[i][j]).item<bool>()) {
+                ub += 1;
+            }
+            ans.index_put_({i, j}, ub);
+        }
+    }
+    return ans;
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("upper_bound", &upper_bound, "Upper Bound");
     m.def("upper_bound_par", &upper_bound_par, "Upper Bound Parallel");
+    m.def("upper_bound_full", &upper_bound_full, "Upper Bound for all nodes.");
+    m.def("upper_bound_full_par", &upper_bound_full_par,
+          "Upper Bound Parallel for all nodes.");
 }
