e367d9c63072f929c14817db8a8544fe7425fdd1
doujiang-zheng <1825597835@qq.com>
2020-08-19 21:38:51 +0800

    Move TSAGEConv from util_dgl.py to layers.py.

--- torch_model/layers.py
+++ torch_model/layers.py
@@ -86,7 +86,7 @@ class TemporalLinkLayer(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, g, src_eids, dst_eids, t):
-        """For each `(u, v, t)`, we get embedding_u by 
+        """For each `(u, v, t)`, we get embedding_u by
         `g.edata['src_feat'][src_eids]`, get embedding_v by
         `g.edata['dst_feat'][dst_eids]`.
 
@@ -120,3 +120,188 @@ class TemporalNodeLayer(nn.Module):
         x = g.edata[f"{mode}_feat"][eids]
         x = self.encode_time(x)
         return self.fc(self.dropout(x))
+
+
+class TSAGEConv(nn.Module):
+    r"""Temporally GraphSAGE layer means aggregation only performing over the valid temporal neighbors. And each edge get distinct embeddings for source node and destionation node. Finally, we return the edge emebddings for all nodes at different timestamps, whose space cost is O(2*E*dim).
+
+    All params remain the same as ``SAGEConv`` in ``dgl.nn.pytorch.conv.sagecong.py``.
+
+    Parameters
+    ----------
+    in_feats : int, or pair of ints
+    out_feats : int
+    feat_drop : float
+    aggregator_type : str
+        Aggregator type to use (``mean``, ``gcn``, ``pool``, ``lstm``).
+    bias : bool
+    norm : callable activation function/layer or None, optional
+    activation : callable activation function/layer or None, optional
+    """
+
+    def __init__(self, in_feats, out_feats, aggregator_type, feat_drop=0.,
+                 time_encoding="cosine"):
+        super(TSAGEConv, self).__init__()
+
+        self._in_src_feats, self._in_dst_feats = in_feats, in_feats
+        self._out_feats = out_feats
+        self._aggre_type = aggregator_type
+        self.feat_drop = nn.Dropout(feat_drop)
+        self.encode_time = TimeEncodingLayer(
+            in_feats, in_feats, time_encoding=time_encoding)
+        # aggregator type: mean/pool/lstm/gcn
+        if aggregator_type == 'pool':
+            self.fc_pool = nn.Linear(self._in_src_feats, self._in_src_feats)
+        if aggregator_type == 'lstm':
+            self.lstm = nn.LSTM(self._in_src_feats,
+                                self._in_src_feats, batch_first=True)
+        if aggregator_type != 'gcn':
+            self.fc_self = nn.Linear(self._in_dst_feats, out_feats)
+        self.fc_neigh = nn.Linear(self._in_src_feats, out_feats)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        """Reinitialize learnable parameters."""
+        gain = nn.init.calculate_gain('relu')
+        if self._aggre_type == 'pool':
+            nn.init.xavier_uniform_(self.fc_pool.weight, gain=gain)
+        if self._aggre_type == 'lstm':
+            self.lstm.reset_parameters()
+        if self._aggre_type != 'gcn':
+            nn.init.xavier_uniform_(self.fc_self.weight, gain=gain)
+        nn.init.xavier_uniform_(self.fc_neigh.weight, gain=gain)
+
+    def _lstm_reducer(self, edge_feat):
+        """LSTM processing for temporal edges.
+        """
+        # (seq_len, batch_size, dim) <= (bucket_size, deg, dim)
+        edge_feat = edge_feat.permute(1, 0, 2)
+        batch_size = edge_feat.shape[1]
+        h = (edge_feat.new_zeros((1, batch_size, self._in_src_feats)),
+             edge_feat.new_zeros((1, batch_size, self._in_src_feats)))
+        rst, (h_, c_) = self.lstm(edge_feat, h)
+        return rst
+
+    def group_func_wrapper(self, groupby, src_feat, dst_feat):
+        """Set the group by function. The `onehop_conv` performs different aggregations over all valid temporal neighbors. The final embeddings are stored in the edges, named `src_feat` or `dst_feat`.
+
+        Parameters:
+        ------------
+        deg_indices: Pre-computed index matrices for batch nodes. It is stored as a dictionary, with degree as keys, and the index matrix as values. It differs in source groupby and destination groupby modes.
+        """
+
+        if groupby not in ["src", "dst"]:
+            raise NotImplementedError
+
+        def onehop_conv(edges):
+            # The first layer is a combination of ndoe features and edge features.
+            h_self, h_neighs = edges.data[src_feat], edges.data[dst_feat]
+            deg_self, deg_neighs = edges.src["deg"], edges.dst["deg"]
+            if groupby == "dst":
+                h_self, h_neighs = h_neighs, h_self
+                deg_self, deg_neighs = deg_neighs, deg_self
+            assert h_self.shape == h_neighs.shape
+
+            # print("bucket shape", h_self.shape)
+            buc, deg, dim = h_self.shape
+            # Attention! There are edges with the same timestamp. The lower triangular assumption is not hold, and we comment the following codes.
+            # assert the timestamp is increasing
+            # orders = torch.argsort(edges.data["timestamp"], dim=1)
+            # assert torch.all(torch.eq(torch.arange(deg).to(orders), orders))
+            # mask = torch.tril(torch.ones(deg, deg)).to(h_neighs)
+            # sum over all valid neighbors: (bucket_size, deg, dim)
+            # mask_feat = torch.matmul(mask, h_neighs) / mask.sum(dim=-1, keepdim=True)
+            # (B, Deg, 1) if the last dimension is 1
+            ts = edges.data["timestamp"].view(buc, deg, 1)
+            # The following mask matrix would crush out of CUDA memory. For the
+            # 58k degree node, it consumes 12GB memory.
+            # mask = (ts.permute(0, 2, 1) <= ts).float()  # (B, Deg, Deg)
+            # We assume the batch mechanism keeps stable during training.
+            # (bucket, deg, dim)
+            indices = edges.data[f"{groupby}_deg_indices"].expand(-1, -1, dim)
+
+            if self._aggre_type == "mean":
+                # mask_feat = torch.bmm(mask, h_neighs)
+                # mask_feat = mask_feat / mask.sum(dim=-1, keepdim=True)
+                # mean_cof = torch.arange(deg).add_(1.0).unsqueeze_(-1)
+                mean_cof = edges.data[f"{groupby}_deg_indices"].add(
+                    1.0).view(buc, deg, 1)
+                h_feat = h_neighs.cumsum(dim=1) / mean_cof
+                mask_feat = h_feat.gather(dim=1, index=indices)
+            elif self._aggre_type == "gcn":
+                # mask_feat = torch.bmm(mask, h_neighs)
+                h_feat = h_neighs.cumsum(dim=1)
+                mask_feat = h_feat.gather(dim=1, index=indices)
+                norm_cof = deg_self.to(mask_feat) + 1
+                mask_feat = (mask_feat + h_self) / norm_cof.unsqueeze(-1)
+            elif self._aggre_type == "pool":
+                # mask_feat = torch.bmm(mask, h_neighs)
+                # mask_feat = mask_feat / mask.sum(dim=-1, keepdim=True)
+                # Since we get (upper_bound - 1) indices, we can use cummax() + gather() to perform max_pooling operation.
+                h_neighs = F.relu(self.fc_pool(h_neighs))
+                h_feat = h_neighs.cummax(dim=1).values
+                mask_feat = h_feat.gather(dim=1, index=indices)
+            elif self._aggre_type == 'lstm':
+                raise NotImplementedError
+            else:
+                raise NotImplementedError
+
+            if self._aggre_type == "gcn":
+                rst = self.fc_neigh(mask_feat)
+            else:
+                rst = self.fc_self(h_self) + self.fc_neigh(mask_feat)
+
+            return {f'{groupby}_feat': rst}
+        return onehop_conv
+
+    def forward(self, graph, current_layer=1):
+        r"""We utilize ``dgl.DGLGraph.group_apply_edges`` to compute TGraphSAGE layer.
+
+        Parameters
+        ----------
+        graph : DGLGraph
+            The graph.
+
+        current_layer : int, default as `1`.
+            As we compute embeddings for each node at its each edge, the count of total embeddings are ``4 * E`` (E is the number of edges), comprise of source node embeddings and destination embeddings.
+            In the 1st layer, we accesses the previous layer embeddings via node features, whose shape is also ``O(E * dim)``.
+            In the next layers, we accesses the previous layer embeddings via ``EdgeBatch.data["src_feat%d"%(current_layer-1)]``.
+
+        Returns
+        ----------
+        src_feat : Tensor
+        dst_feat : Tensor
+        """
+        g = graph.local_var()
+
+        src_name = f'src_feat{current_layer - 1}'
+        dst_name = f'dst_feat{current_layer - 1}'
+        # add dropout layer for node embeddings
+        src_feat, dst_feat = g.edata[src_name], g.edata[dst_name]
+        src_feat = self.encode_time(src_feat, g.edata["timestamp"])
+        dst_feat = self.encode_time(dst_feat, g.edata["timestamp"])
+        src_feat, dst_feat = self.feat_drop(src_feat), self.feat_drop(dst_feat)
+        g.edata[src_name] = src_feat
+        g.edata[dst_name] = dst_feat
+
+        src_conv = self.group_func_wrapper(
+            groupby="src",  src_feat=src_feat, dst_feat=dst_feat)
+        dst_conv = self.group_func_wrapper(
+            groupby="dst",  src_feat=src_feat, dst_feat=dst_feat)
+        g.group_apply_edges(group_by="src", func=src_conv)
+        g.group_apply_edges(group_by="dst", func=dst_conv)
+
+        src_feat, dst_feat = g.edata["src_feat"], graph.edata["dst_feat"]
+        return src_feat, dst_feat
+
+
+class GTAConv(TSAGEConv):
+    pass
+
+
+class TGATConv(nn.Module):
+    pass
+
+
+class GTRConv(nn.Module):
+    pass
--- torch_model/util_dgl.py
+++ torch_model/util_dgl.py
@@ -6,22 +6,14 @@ import sys
 import time
 
 import dgl
-import dgl.function as fn
-import dgl.nn.pytorch as dglnn
-from dgl.nn.pytorch.conv import SAGEConv
-from dgl.utils import expand_as_pair, check_eq_shape
-import networkx as nx
 import numpy as np
 import pandas as pd
-import scipy.sparse as ssp
 import torch
-import torch as th
 import torch.nn as nn
 from torch.nn import functional as F
-from tqdm import trange
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 
 from model.utils import get_free_gpu
+from torch_model.layers import TSAGEConv
 
 
 def parse_args():
@@ -61,171 +53,6 @@ def timeit(method):
         print("%r  %2.2f s" % (method.__name__, te - ts))
         return result
     return timed
-
-
-class TSAGEConv(SAGEConv):
-    r"""Temporally GraphSAGE layer means aggregation only performing over the valid temporal neighbors. And each edge get distinct embeddings for source node and destionation node. Finally, we return the edge emebddings for all nodes at different timestamps, whose space cost is O(2*E*dim).
-
-    All params remain the same as ``SAGEConv`` in ``dgl.nn.pytorch.conv.sagecong.py``.
-
-    Parameters
-    ----------
-    in_feats : int, or pair of ints
-    out_feats : int
-    feat_drop : float
-    aggregator_type : str
-        Aggregator type to use (``mean``, ``gcn``, ``pool``, ``lstm``).
-    bias : bool
-    norm : callable activation function/layer or None, optional
-    activation : callable activation function/layer or None, optional
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(TSAGEConv, self).__init__(*args, **kwargs)
-
-    def _lstm_reducer(self, edge_feat):
-        """LSTM processing for temporal edges.
-        """
-        # (seq_len, batch_size, dim) <= (bucket_size, deg, dim)
-        edge_feat = edge_feat.permute(1, 0, 2)
-        batch_size = edge_feat.shape[1]
-        h = (edge_feat.new_zeros((1, batch_size, self._in_src_feats)),
-             edge_feat.new_zeros((1, batch_size, self._in_src_feats)))
-        rst, (h_, c_) = self.lstm(edge_feat, h)
-        return rst
-
-    def group_func_wrapper(self, groupby, src_feat, dst_feat):
-        """Set the group by function. The `onehop_conv` performs different aggregations over all valid temporal neighbors. The final embeddings are stored in the edges, named `src_feat` or `dst_feat`.
-
-        Parameters:
-        ------------
-        deg_indices: Pre-computed index matrices for batch nodes. It is stored as a dictionary, with degree as keys, and the index matrix as values. It differs in source groupby and destination groupby modes.
-        """
-        def onehop_conv(edges):
-            if src_feat.startswith("src_feat"):
-                h_self = edges.data[src_feat]
-                h_neighs = edges.data[dst_feat]
-            else:
-                h_self = edges.src[src_feat]
-                h_neighs = edges.dst[dst_feat]
-            if groupby not in ["src", "dst"]:
-                raise NotImplementedError
-            deg_self, deg_neighs = edges.src["deg"], edges.dst["deg"]
-            if groupby == "dst":
-                h_self, h_neighs = h_neighs, h_self
-                deg_self, deg_neighs = deg_neighs, deg_self
-            assert h_self.shape == h_neighs.shape
-            # add dropout layer for node embeddings
-            h_self, h_neighs = self.feat_drop(h_self), self.feat_drop(h_neighs)
-            # print("bucket shape", h_self.shape)
-            buc, deg, dim = h_self.shape
-            # Attention! There are edges with the same timestamp. So the lower triangular assumption is not hold. So we comment the following codes.
-            # assert the timestamp is increasing
-            # orders = torch.argsort(edges.data["timestamp"], dim=1)
-            # assert torch.all(torch.eq(torch.arange(deg).to(orders), orders))
-            # mask = torch.tril(torch.ones(deg, deg)).to(h_neighs)
-            # mask = self.feat_drop(mask)
-            # sum over all valid neighbors: (bucket_size, deg, dim)
-            # mask_feat = torch.matmul(mask, h_neighs) / mask.sum(dim=-1, keepdim=True)
-            # (B, Deg, 1) if the last dimension is 1
-            ts = edges.data["timestamp"].view(buc, deg, 1)
-            # The mask matrix would crush out of CUDA memory. For the 58k degree node, it consumes 12GB memory.
-            # mask = (ts.permute(0, 2, 1) <= ts).float()  # (B, Deg, Deg)
-            # We assume the batch mechanism keeps stable during training.
-            # (bucket, deg, dim)
-            indices = edges.data[f"{groupby}_deg_indices"].expand(-1, -1, dim)
-
-            if self._aggre_type == "mean":
-                # mask_feat = torch.bmm(mask, h_neighs)
-                # mask_feat = mask_feat / mask.sum(dim=-1, keepdim=True)
-                # mean_cof = torch.arange(deg).add_(1.0).unsqueeze_(-1)
-                mean_cof = edges.data[f"{groupby}_deg_indices"].add(
-                    1.0).view(buc, deg, 1)
-                h_feat = h_neighs.cumsum(dim=1) / mean_cof
-                mask_feat = h_feat.gather(dim=1, index=indices)
-            elif self._aggre_type == "gcn":
-                # mask_feat = torch.bmm(mask, h_neighs)
-                h_feat = h_neighs.cumsum(dim=1)
-                mask_feat = h_feat.gather(dim=1, index=indices)
-                norm_cof = deg_self.to(mask_feat) + 1
-                mask_feat = (mask_feat + h_self) / norm_cof.unsqueeze(-1)
-            elif self._aggre_type == "pool":
-                # mask_feat = torch.bmm(mask, h_neighs)
-                # mask_feat = mask_feat / mask.sum(dim=-1, keepdim=True)
-                # Since we get (upper_bound - 1) indices, we can use cummax() + gather() to perform max_pooling operation.
-                h_neighs = F.relu(self.fc_pool(h_neighs))
-                h_feat = h_neighs.cummax(dim=1).values
-                mask_feat = h_feat.gather(dim=1, index=indices)
-            elif self._aggre_type == 'lstm':
-                raise NotImplementedError
-            else:
-                raise NotImplementedError
-
-            if self._aggre_type == "gcn":
-                rst = self.fc_neigh(mask_feat)
-            else:
-                rst = self.fc_self(h_self) + self.fc_neigh(mask_feat)
-                # rst = rst / mask.sum(dim=-1, keepdim=True)
-
-            if self.activation is not None:
-                rst = self.activation(rst)
-            if self.norm is not None:
-                rst = self.norm(rst)
-            return {f'{groupby}_feat': rst}
-        return onehop_conv
-
-    def forward(self, graph, current_layer=1):
-        r"""We utilize ``dgl.DGLGraph.group_apply_edges`` to compute TGraphSAGE layer.
-
-        Parameters
-        ----------
-        graph : DGLGraph
-            The graph.
-
-        current_layer : int, default as `1`.
-            As we compute embeddings for each node at its each edge, the count of total embeddings are ``2 * E`` (E is the number of edges), comprise of source node embeddings and destination embeddings. 
-            In the 1st layer, we accesses the previous layer embeddings via node features, whose shape is also ``O(E * dim)``.
-            In the next layers, we accesses the previous layer embeddings via ``EdgeBatch.data["src_feat%d"%(current_layer-1)]``.
-
-        src_feat_name : str
-        dst_feat_name : str
-
-        Returns
-        ----------
-        src_feat : Tensor
-        dst_feat : Tensor
-        """
-        graph = graph.local_var()
-        # compute for gcn normalization
-        graph.ndata["deg"] = (graph.in_degrees() +
-                              graph.out_degrees()).to(graph.ndata["nfeat"])
-        if current_layer >= 2:
-            src_feat = f'src_feat{current_layer - 1}'
-            dst_feat = f'dst_feat{current_layer - 1}'
-            dim = graph.edata[src_feat].shape[-1]
-        else:
-            src_feat = dst_feat = "nfeat"
-            dim = graph.ndata[src_feat].shape[-1]
-
-        src_conv = self.group_func_wrapper(
-            groupby="src",  src_feat=src_feat, dst_feat=dst_feat)
-        dst_conv = self.group_func_wrapper(
-            groupby="dst",  src_feat=src_feat, dst_feat=dst_feat)
-        graph.group_apply_edges(group_by="src", func=src_conv)
-        graph.group_apply_edges(group_by="dst", func=dst_conv)
-        return graph.edata["src_feat"], graph.edata["dst_feat"]
-
-
-class GTAConv(TSAGEConv):
-    pass
-
-
-class TGATConv(nn.Module):
-    pass
-
-
-class GTRConv(nn.Module):
-    pass
 
 
 def construct_dglgraph(edges, nodes, device, node_dim=128, bidirected=False):
