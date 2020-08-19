733d12f480a681cf39679268d4d0d040bf2161c8
doujiang-zheng <1825597835@qq.com>
2020-08-19 10:59:12 +0800

    Add refer_latest_edge method, computing the latest edge id for each (u, t).

--- torch_model/upper_bound_extension/upper_bound.cpp
+++ torch_model/upper_bound_extension/upper_bound.cpp
@@ -191,32 +191,62 @@ void sub_upper_bound_full(const torch::Tensor &etime, torch::Tensor &ans,
     }
 }
 
-torch::Tensor reference_lower_bound(const torch::Tensor &refer_adj,
-                                    const torch::Tensor &bucket_adj) {
-    /*  First of all, the last dimension of bucket_adj must be
-     * non-decreasing. We compute the upper_bound of each element along the
-     * last dimension. The return result will be the same shape as input
-     * `bucket_adj`.
+torch::Tensor refer_latest_edge(
+    const torch::Tensor &refer_nodes, const torch::Tensor &refer_degrees,
+    const torch::Tensor &refer_eids, const torch::Tensor &refer_etime,
+    const torch::Tensor &self_nodes, const torch::Tensor &self_degrees,
+    const torch::Tensor &self_eids, const torch::Tensor &self_etime) {
+    /* For each edge in self_eids, we find the corresponding latest node
+     * interaction edge in refer_eids.We assume that `refer_nodes` and
+     * `self_nodes` share the same node ids, and `refer_etime` and
+     * `self_etime` are non-descending for each node `u`. So that we can
+     * calculate the latest refer_eid cumulatively with respect to self_eid.
+     * Detailedly, for `(u, t_i)` and `(u, t_{i+1})`, we have that `eid(u, t_i)
+     * <= eid(u, t_{i+1})` because `t_i <= t_{i+1}`. All eids are initialized as
+     * the first eid of node `u`.
+     *
+     * Parameters
+     * ----------
+     * refer_nodes: 1D tensor, shape (N), stores the node ids.
+     * refer_degrees : 1D tensor, shape (N), stores the corresponding node
+     *      degree for each node. refer_degrees.sum() == refer_eids.sizes()[0].
+     * refer_eids : 1D tensor, stores the cumulative edge ids in DGLGraph for
+     *      each node.
+     * refer_etime : 1D tensor, stores the corresponding edge timestamp.
      */
     using namespace torch::indexing;
     // We assume the bucket_adj is 2D tensor.
-    auto sizes = bucket_adj.sizes();
-    auto dim = sizes[1];
-    // Default upper_bound is the array length, which means itself is the
-    // largest element.
-    auto ans = torch::full(sizes, dim, torch::kInt64);
-    for (auto i = 0; i < sizes[0]; i++) {
-        auto ub = 1;
-        for (auto j = 0; j < dim && ub < dim; j++) {
-            // Attention! If use `bucket_adj[i][ub].item<float>()` to get
-            // float value, the float precision cannot be guaranteed, so we
-            // directly get .item<bool>().
-            while (ub < dim &&
-                   (bucket_adj[i][ub] <= bucket_adj[i][j]).item<bool>()) {
-                ub += 1;
+    auto num_node = self_nodes.sizes()[0];
+    auto num_eids = self_eids.sizes()[0];
+    auto ans = torch::zeros({num_eids}, torch::kInt64);
+    auto ridx = 0, sidx = 0;
+    for (auto i = 0; i < num_node; i++) {
+        auto rdeg = refer_degrees[i].item<int>();
+        auto sdeg = self_degrees[i].item<int>();
+        auto lb = 0;
+        for (auto j = 0; j < sdeg; j++) {
+            while (lb + 1 < rdeg &&
+                   (refer_etime[ridx + lb + 1] < self_etime[sidx + j])
+                       .item<bool>()) {
+                // We define the lower_bound of self_etime[sidx +j] as the
+                // largest refer_etime[ridx + lb] so that `refer_etime[ridx +
+                // lb] < self_etime[sidx + j]`.
+                lb += 1;
             }
-            ans.index_put_({i, j}, ub);
+            ans.index_put_({sidx + j}, refer_eids[ridx + lb]);
+            // if (self_eids[sidx + j].item<int>() == 2738) {
+            //     std::cout << "self id: " << sidx + j << " refer to "
+            //               << refer_eids[ridx + lb].item<double>() <<
+            //               std::endl;
+            //     std::cout << self_etime[sidx + j].item<double>() << " > "
+            //               << refer_etime[ridx + lb].item<double>()
+            //               << (self_etime[sidx + j] > refer_etime[ridx + lb])
+            //                      .item<bool>()
+            //               << std::endl;
+            // }
         }
+        ridx += rdeg;
+        sidx += sdeg;
     }
     return ans;
 }
@@ -227,4 +257,5 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("upper_bound_full", &upper_bound_full, "Upper Bound for all nodes.");
     m.def("upper_bound_full_par", &upper_bound_full_par,
           "Upper Bound Parallel for all nodes.");
+    m.def("refer_latest_edge", &refer_latest_edge, "Refer Latest Edge.");
 }
