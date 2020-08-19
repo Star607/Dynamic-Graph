a9774864b9ae2681f61b89ed8586578c660687d5
doujiang-zheng <1825597835@qq.com>
2020-08-19 19:56:06 +0800

    Not complete: add train, valid, test, and early_stopping procedures.

--- torch_model/models.py
+++ torch_model/models.py
@@ -20,7 +20,7 @@ from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 from data_loader.data_util import load_data, load_split_edges, load_label_edges
 from model.utils import get_free_gpu, timeit, EarlyStopMonitor
 from torch_model.util_dgl import set_logger, construct_dglgraph, padding_node, TSAGEConv
-from torch_model.layers import TemporalLinkLayer, LatestNodeInteractionFinder
+from torch_model.layers import TemporalLinkLayer
 from torch_model.eid_precomputation import _prepare_deg_indices, _par_deg_indices, _deg_indices_full, _par_deg_indices_full, _latest_edge, LatestNodeInteractionFinder
 # A cpp extension computing upper_bound along the last dimension of an non-decreasing matrix. It saves huge memory use.
 import upper_bound_cpp
@@ -42,12 +42,14 @@ class TGraphSAGE(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.activation = activation
 
-    def forward(self, g, features):
-        """In the 1st layer, we use the node features/embeddings as the features for each edge.
-        In the next layers, we store the edge features in the edges, named `src_feat{current_layer}` and `dst_feat{current_layer}`.
+    def forward(self, g):
+        """In the 1st layer, we use the node features/embeddings as the features
+        for each edge. In the next layers, we store the edge features in the edges,
+        named `src_feat{current_layer}` and `dst_feat{current_layer}`.
         """
         g = g.local_var()
-        g.ndata["nfeat"] = features
+        # g.edata["src_feat0"] = ...
+        # g.edata["dst_feat0"] = ...
         for i, layer in enumerate(self.layers):
             # print(f"layer {i}")
             cl = i + 1
@@ -57,20 +59,61 @@ class TGraphSAGE(nn.Module):
         return src_feat, dst_feat
 
 
-class TemporalLinkTrainer(nn.Module):
-    def __init__(self, in_feats, args):
-        super(TemporalLinkTrainer, self).__init__()
-        self.conv = TGraphSAGE(in_feats, args.n_hidden, in_feats,
-                               args.n_layers, F.relu, args.dropout, args.agg_type)
-        self.pred = TemporalLinkLayer(in_feats, 1, time_encoding=args.time_encoding)
+class NegativeSampler(object):
+    def __init__(self, g, k):
+        self.weights = g.in_degrees().float() ** 0.75
+        self.k = k
 
-    def forward(self, g, features, label_edges, bidirected=False):
+    def __call__(self, g, eids):
+        src, _ = g.find_edges(eids)
+        n = len(src)
+        dst = self.weights.multinomial(self.k * n, replacement=True)
+        src = src.repeat_interleave(self.k * n)
+        return src, dst
+
+
+class HistorySampler(object):
+    def __init__(self):
+        pass
+
+    def __call__(self, g, eids):
+        pass
+
+
+class TemporalLinkTrainer(nn.Module):
+    def __init__(self, g, in_feats, args):
+        super(TemporalLinkTrainer, self).__init__()
+        self.logger = logging.getLogger()
+        if g.ndata["nfeat"].requires_grad:
+            self.nfeat = g.ndata["nfeat"]
+            self.logger.info(
+                "Optimization includes randomly initialized dim-{} node \
+                embeddings.".format(self.nfeat.shape[-1]))
+        else:
+            self.logger.info(
+                "Optimization only includes convolution parameters.")
+
+        self.conv = TGraphSAGE(in_feats, args.n_hidden, args.n_hidden,
+                               args.n_layers, F.relu, args.dropout, args.agg_type)
+        self.pred = TemporalLinkLayer(
+            in_feats, 1, time_encoding=args.time_encoding)
+        self.loss_fn = nn.BCEWithLogitsLoss()
+        self.n_neg = args.n_neg
+        self.n_hist = args.n_hist
+        self.neg_sampler = NegativeSampler(g, self.n_neg)
+        self.his_sampler = HistorySampler()
+
+    def forward(self, g, batch_eids, bidirected=False):
         g = g.local_var()
-        src_feat, dst_feat = self.conv(g, features)
+        src_feat, dst_feat = self.conv(g)
         g.edata["src_feat"] = src_feat
         g.edata["dst_feat"] = dst_feat
-        logits = self.pred(g, label_edges, bidirected=bidirected)
+        # self.pred(g, batch_eids, bidirected=bidirected)
+        logits = (src_feat[batch_eids] * dst_feat[batch_eids]).sum(dim=-1)
         return logits.squeeze()
+
+    def contrastive(self, g, batch_eids):
+        pass
 
     def infer(self, g, features, edges, bidirected=False):
         pass
@@ -107,7 +150,8 @@ def _check_upper_bound_extension(g):
         for key in deg_indices.keys():
             assert torch.equal(deg_indices[key], par_deg_indices[key]), key
             assert torch.equal(deg_indices[key], full_deg_indices[key]), key
-            assert torch.equal(deg_indices[key], par_full_deg_indices[key]), key
+            assert torch.equal(
+                deg_indices[key], par_full_deg_indices[key]), key
 
 
 def _check_latest_edge_extension(g, edges):
@@ -121,90 +165,146 @@ def _check_latest_edge_extension(g, edges):
         pyeids = LatestNodeInteractionFinder(g, v, t, mode="out")
         cppeids = _latest_edge(g, v, t, mode="out")
         assert torch.equal(pyeids, cppeids)
-    pass
 
 
 def _df2np(edges):
     return edges.iloc[:, :3].to_numpy().transpose()
 
 
-def train(args):
-    pass
+def eval_linkpred(model, g, df, batch_size=None):
+    if batch_size is None:
+        batch_size = df.shape[0]
+    model.eval()
+    with torch.no_grad():
+        logits = model.infer(
+            g, _df2np(df.iloc[:batch_size]))
+        logits = torch.sigmoid(logits).cpu().numpy()
+        labels = df["label"].iloc[:batch_size]
+        acc = accuracy_score(labels, logits >= 0.5)
+        f1 = f1_score(labels, logits >= 0.5)
+        auc = roc_auc_score(labels, logits)
+    return acc, f1, auc
+
+
+def write_result(val_auc, metrics, dataset, params):
+    res_path = "result/{}-GTC.csv".format(dataset)
+    headers = ["method", "dataset", "valid_auc",
+               "accuracy", "f1", "auc", "params"]
+    acc, f1, auc = metrics
+    if not os.path.exists(res_path):
+        f = open(res_path, 'w')
+        f.write(",".join(headers) + "\r\n")
+        f.close()
+        os.chmod(res_path, 0o777)
+    with open(res_path, 'a') as f:
+        result_str = "GTC,{},{:.4f},{:.4f},{:.4f},{:.4f}".format(
+            dataset, val_auc, acc, f1, auc)
+        params_str = ",".join(["{}={}".format(k, v)
+                               for k, v in params.items()])
+        params_str = "\"{}\"".format(params_str)
+        row = result_str + "," + params_str + "\r\n"
+        f.write(row)
 
 
 def main():
+    # Set arg_parser, logger, and etc.
     args = parse_args()
     logger = set_logger()
     logger.info(args)
-    nodes, edges, train_labels, val_labels, test_labels = prepare_dataset(args.dataset)
 
-    delta = edges["timestamp"].shift(-1) - edges["timestamp"]
-    # pandas loc[low:high] includes high
-    assert np.all(delta[:len(delta) - 1] >= 0)
-
+    # Set device utility.
     if args.gpu:
         if args.gid >= 0:
             device = torch.device("cuda:{}".format(args.gid))
         else:
             device = torch.device("cuda:{}".format(get_free_gpu()))
+        logger.info("Begin Conv on Device %s, GPU Memory %d GB", device,
+                    torch.cuda.get_device_properties(device).total_memory // 2**30)
     else:
         device = torch.device("cpu")
+
+    # Load nodes, edges, and labeled dataset for training, validation and test.
+    nodes, edges, train_labels, val_labels, test_labels = prepare_dataset(
+        args.dataset)
+    delta = edges["timestamp"].shift(-1) - edges["timestamp"]
+    # Pandas loc[low:high] includes high, so we use slice operations here instead.
+    assert np.all(delta[:len(delta) - 1] >= 0)
+
+    # Set DGLGraph, node_features, edge_features, and edge timestamps.
     g = construct_dglgraph(edges, nodes, device, bidirected=args.bidirected)
-    _check_latest_edge_extension(g, train_labels)
-    exit(0)
+    # For each entry in the adjacency list `u: (v, t)` of node `u`, compute
+    # the related upper_bound with respect to `t`. So that we can use `cumsum`
+    # or `cummax` to accelerate the computation speed. Otherwise, we have to
+    # compute a mask matrix multiplication for each `(v, t)`, which costs even
+    # 12GB memory for 58K interactions.
     deg_indices = _par_deg_indices_full(g)
     for k, v in deg_indices.items():
         g.edata[k] = v.to(device).unsqueeze(-1).detach()
 
-    logger.info("Begin Conv on Device %s, GPU Memory %d GB", device,
-                torch.cuda.get_device_properties(device).total_memory // 2**30)
-
-    in_feats = g.ndata["nfeat"].shape[-1]
+    # Set model configuration.
+    # Input features: node_featurs + edge_features + time_encoding
+    in_feats = (g.ndata["nfeat"].shape[-1] + g.edata["efeat"].shape[-1])
     out_feats = args.n_hidden
-    n_edges = g.number_of_edges()
-    model = TemporalLinkTrainer(in_feats, args)
+    model = TemporalLinkTrainer(g, in_feats, args)
     model = model.to(device)
-    loss_fn = nn.BCEWithLogitsLoss()
-    if g.ndata["nfeat"].requires_grad:
-        logger.info(
-            "Optimization includes randomly initialized dim-{} node embeddings.".format(g.ndata["nfeat"].shape[-1]))
-        params = itertools.chain([g.ndata["nfeat"]], model.parameters())
-    else:
-        logger.info("Optimization only includes convolution parameters.")
-        params = model.parameters()
     optimizer = torch.optim.Adam(
-        params, lr=args.lr, weight_decay=args.weight_decay)
+        model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
-    # for epoch in range(args.epochs):
+    # only use positive edges
+    train_eids = np.arange(train_labels.shape[0] // 2)
+    num_batch = np.int(np.ceil(train_eids / args.batch_size))
     epoch_bar = trange(args.epochs, disable=(not args.display))
+    early_stopper = EarlyStopMonitor(max_round=10)
     for epoch in epoch_bar:
-        model.train()
-        logits = model(g, g.ndata["nfeat"], _df2np(train_labels))
-        labels = torch.tensor(train_labels["label"]).float().to(device)
-        loss = loss_fn(logits, labels)
+        np.random.shuffle(train_eids)
+        batch_bar = trange(num_batch, disable=(not args.display))
+        for idx in batch_bar:
+            model.train()
+            batch_eids = train_eids[idx * args.batch_size:
+                                    (idx + 1) * args.batch_size]
+            loss = model(g, batch_eids)
+            optimizer.zero_grad()
+            loss.backward()
+            # clip gradients by value: https://stackoverflow.com/questions/54716377/how-to-do-gradient-clipping-in-pytorch
+            for p in model.parameters():
+                p.register_hook(lambda grad: torch.clamp(
+                    grad, -args.clip, args.clip))
+            optimizer.step()
 
-        optimizer.zero_grad()
-        loss.backward()
-        # clip gradients by value: https://stackoverflow.com/questions/54716377/how-to-do-gradient-clipping-in-pytorch
-        for p in params:
-            p.register_hook(lambda grad: torch.clamp(
-                grad, -args.clip, args.clip))
-        optimizer.step()
+            acc, f1, auc = eval_linkpred(
+                model, g, val_labels, batch_size=args.batch_size)
+            batch_bar.set_postfix(loss=loss.item(), acc=acc, f1=f1, auc=auc)
 
-        model.eval()
-        with torch.no_grad():
-            logits = model(g, g.ndata["nfeat"], _df2np(val_labels))
-            logits = torch.sigmoid(logits).cpu().numpy()
-            labels = val_labels["label"]
-            acc = accuracy_score(labels, logits >= 0.5)
-            f1 = f1_score(labels, logits >= 0.5)
-            auc = roc_auc_score(labels, logits)
-
+        acc, f1, auc = eval_linkpred(model, g, val_labels)
         if epoch % 100 == 0:
             logger.info("epoch:%d acc: %.4f, auc: %.4f, f1: %.4f",
                         epoch, acc, auc, f1)
         epoch_bar.update()
         epoch_bar.set_postfix(loss=loss.item(), acc=acc, f1=f1, auc=auc)
+
+        def ckpt_path(
+            epoch): return f'./ckpt/{args.dataset}-{args.agg_type}-{epoch}.pth'
+        if early_stopper.early_stop_check(auc):
+            logger.info(
+                f"No improvement over {early_stopper.max_round} epochs.")
+            logger.info(
+                f'Loading the best model at epoch {early_stopper.best_epoch}')
+            model.load_state_dict(torch.load(
+                ckpt_path(early_stopper.best_epoch)))
+            logger.info(
+                f'Loaded the best model at epoch {early_stopper.best_epoch} for inference')
+            break
+        else:
+            torch.save(model.state_dict(), ckpt_path(epoch))
+    model.eval()
+    _, _, val_auc = eval_linkpred(model, g, val_labels)
+    acc, f1, auc = eval_linkpred(model, g, test_labels)
+    params = {"epochs": early_stopper.best_epoch,
+              "bidirected": args.bidirected, "n_hist": args.n_hist,
+              "n_neg": args.n_neg, "n_layers": args.n_layers,
+              "time_encoding": args.time_encoding, "dropout": args.dropout,
+              "weight_decay": args.weight_decay}
+    write_result(val_auc, (acc, f1, auc), args.dataset, params)
 
 
 def parse_args():
@@ -227,18 +327,23 @@ def parse_args():
     parser.add_argument("--epochs", type=int, default=100,
                         help="number of training epochs")
     parser.add_argument("--time-encoding", "-te", type=str, default="cosine",
-                        help="Time encoding function.")
+                        help="Time encoding function.", choices=["concat", "cosine", "outer"])
     parser.add_argument("--batch-size", type=int, default=1024)
     parser.add_argument("--n-hidden", type=int, default=128,
                         help="number of hidden gcn units")
     parser.add_argument("--n-layers", type=int, default=2,
                         help="number of hidden gcn layers")
+    parser.add_argument("--n-neg", type=int, default=1,
+                        help="number of negative samples")
+    parser.add_argument("--contrastive", action="store_true")
+    parser.add_argument("--n-hist", type=int, default=1,
+                        help="number of history samples")
     parser.add_argument("--weight-decay", type=float, default=1e-5,
                         help="Weight for L2 loss")
     parser.add_argument("--clip", type=float, default=5.0,
                         help="Clip gradients by value.")
     parser.add_argument("--agg-type", type=str, default="gcn",
-                        help="Aggregator type: mean/gcn/pool/lstm")
+                        help="Aggregator type: mean/gcn/pool/lstm/cosine")
     parser.add_argument("--display", dest="display", action="store_true")
     parser.add_argument("--no-display", dest="display", action="store_false"
                         )
